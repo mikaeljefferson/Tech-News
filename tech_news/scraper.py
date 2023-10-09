45c6e3a8e33246becb7d79d28563b4494ec11b55
mikael <mikaeljeffeson@gmail.com>
2023-10-09 09:07:36 -0300

    Feat: Req 01,02,03

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,19 +1,46 @@
+import requests
+import time
+from bs4 import BeautifulSoup
+
+
 # Requisito 1
-def fetch(url):
+def fetch(URL: str):
     """Seu código deve vir aqui"""
-    raise NotImplementedError
+    time.sleep(1)
+    headers = {"user-agent": "Fake user-agent"}
+    try:
+        res = requests.get(URL, headers=headers, timeout=3)
+        if res.status_code == 200:
+            return res.text
+        return None
+
+    except requests.Timeout:
+        return None
+    except Exception as e:
+        print(e)
+        return None
 
 
 # Requisito 2
-def scrape_updates(html_content):
+def scrape_updates(html_content: str) -> list:
     """Seu código deve vir aqui"""
-    raise NotImplementedError
+    soup = BeautifulSoup(html_content, "html.parser")
+    result = []
+    for quote in soup.find_all("header", {"class": "entry-header"}):
+        tag = quote.h2.a["href"]
+        result.append(tag)
+
+    return result
 
 
 # Requisito 3
-def scrape_next_page_link(html_content):
+def scrape_next_page_link(html_content: str) -> str:
     """Seu código deve vir aqui"""
-    raise NotImplementedError
+    soup = BeautifulSoup(html_content, "html.parser")
+    next_page = soup.find("a", {"class": "next page-numbers"})
+    if next_page:
+        return next_page["href"]
+    return None
 
 
 # Requisito 4
