424de1a24baaced6d747deeb3583b24dd3f8b509
mikael <mikaeljeffeson@gmail.com>
2023-10-12 10:05:08 -0300

    Feat:req 7,8,9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,16 +1,33 @@
+from tech_news.database import db
+from datetime import datetime
+
+
 # Requisito 7
 def search_by_title(title):
     """Seu código deve vir aqui"""
-    raise NotImplementedError
+    result = db.news.find({"title": {"$regex": title, "$options": "i"}})
+
+    return [(news["title"], news["url"]) for news in result]
 
 
 # Requisito 8
 def search_by_date(date):
     """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        date_obj = datetime.strptime(date, "%Y-%m-%d")
+        date_str = date_obj.strftime("%d/%m/%Y")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    results = db.news.find({"timestamp": {"$regex": f"^{date_str}"}})
+    return [(news["title"], news["url"]) for news in results]
 
 
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
-    raise NotImplementedError
+    results = db.news.find()
+    filtered_results = filter(
+        lambda news: category.lower() in news['category'].lower(), results
+    )
+    return [(news["title"], news["url"]) for news in filtered_results]
